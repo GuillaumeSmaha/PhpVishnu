3f944dca858b1f962b172f51ba7328b88457fc32
Guillaume Smaha <guillaume.smaha@gmail.com>
2015-03-13 11:30:04 +0100

    Clean project

--- examples/simple/B.php
+++ examples/simple/B.php
@@ -1,27 +1,9 @@
 <?php
 /*!
- * LICENSE
- * 
- *  Copyright (c) 2011 Guillaume Smaha.
- *
- *  PhpVishnu is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation; either version 3,
- *  or (at your option) any later version.
- *
- *  PhpVishnu is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with PhpVishnu; see the file COPYING3. If not see
- *  <http://www.gnu.org/licenses/>.
- * 
  * \package 	PhpVishnu_Example_Simple
- * \copyright	(C) Copyright 2011 Guillaume Smaha.
- * \license		http://www.gnu.org/licenses/ LGPL3
- * \brief		PhpVishnu_Example_Simple is an using example of PhpVishnu.
+ * \author	2011 Guillaume Smaha.
+ * \license	http://www.gnu.org/licenses/ LGPL3
+ * \brief	PhpVishnu_Example_Simple is an using example of PhpVishnu.
  */
 
 set_include_path(get_include_path() . PATH_SEPARATOR . '../../');
